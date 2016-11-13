4876bebeda20bbd802065a4f594c3176f2d27199
annyz <annyz@plumgrid.com>
2016-11-13 23:32:41 +0000

    - Changed keyspace drop for deletion of all rows in table between tests. - Change in jupyter notebook to register write operation results even if reads fail.
    Signed-off-by: annyz <annyz@plumgrid.com>

--- src/hCassandra_test.py
+++ src/hCassandra_test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 __author__ = 'annyz'
 
 from sys import path
@@ -8,6 +9,12 @@ import sys
 import logging
 import math
 import ast
+import copy
+import json
+import threading
+import time
+import random
+import paramiko
 
 from datetime import datetime, timedelta
 from optparse import OptionParser
@@ -15,6 +22,7 @@ from pprint import pformat  # NOQA
 from hydra.lib import util
 from hydra.lib.h_analyser import HAnalyser
 from hydra.lib.hydrabase import HydraBase
+from cassandra.cluster import Cluster
 
 try:
     # Python 2.x
@@ -43,8 +51,8 @@ class RunTestCassandra(HydraBase):
         self.reset_all_app_stats(self.stress_client)
         # Signal message sending
         l.info("Sending signal to Cassandra Stress client to start sending all messages..")
-        # Force start-time for ALL clients +10 seconds from current time
-        start_time = datetime.now() + timedelta(seconds=10)
+        # Force start-time for ALL clients +60 seconds from current time
+        start_time = datetime.now() + timedelta(seconds=60)
         l.debug("Current Time: %s, Start Time: %s" % (datetime.now(), start_time))
         task_list = self.all_task_ids[self.stress_client]
         ha_list = []
@@ -57,8 +65,22 @@ class RunTestCassandra(HydraBase):
             ha_stress.start_test(start_time=start_time)
             ha_list.append(ha_stress)
         l.info('Waiting for test(s) to end...')
-        for ha_stress in ha_list:
+        if self.options.sim_failure:
+            l.debug("Simulate Cassandra Node Failure. Init.")
+            # Thread Event to indicate tests have been completed
+            tests_completed = threading.Event()
+            # Launch parallel Thread to simulate cassandra node failure.
+            l.debug("Launch separate thread to simulate node failure and rejoin.")
+            failure_thread = threading.Thread(target=simulate_node_failure, args=(self.options.cluster_ips.split(','),
+                                                                 self.options.test_duration, tests_completed))
+            failure_thread.start()
+        for idx, ha_stress in enumerate(ha_list):
+            l.debug('Waiting for task [%s] in [%s:%s] test to END. Iteration: %s' % (ha_stress.task_id, ha_stress.server_ip, ha_stress.port, idx))
             ha_stress.wait_for_testend()
+        if self.options.sim_failure:
+            l.debug("ALL tests are COMPLETED.")
+            tests_completed.set()
+        l.info('Fetch App Stats')
         self.fetch_app_stats(self.stress_client)
 
         return self.result_parser()
@@ -66,12 +88,77 @@ class RunTestCassandra(HydraBase):
     def run_test(self, first_run=True):
         # Get Mesos/Marathon Clients
         self.start_init()
+        # Reset (drop) Cassandra DB for cassandra-stress tool default 'keyspace1'
+        self.reset_db()
+        # Create Table(s) & Triggers for stress Test
+        # self.create_triggers()
         # Launch Cassandra Stress-Client(s)
         self.launch_stress_client()
         # Rerun the test
         res = self.rerun_test(self.options)
         # Return Test Results
         return res
+
+    def create_triggers(self):
+        try:
+            cluster_ips = self.options.cluster_ips.split(',')
+            cluster = Cluster(cluster_ips)
+            l.debug("Connecting to Cassandra Cluster: [%s]" % (cluster_ips))
+            session = cluster.connect()
+            l.info("Create keyspace [keyspace1]...")
+            # Create Keyspace
+            session.execute("CREATE KEYSPACE keyspace1 WITH replication = {'class': 'SimpleStrategy', "
+                            "'replication_factor': '1'}  AND durable_writes = true;")
+            l.info("Create tables [standard1] & [counter1]...")
+            table_create = "CREATE TABLE keyspace1.standard1 ( " \
+                           "key blob PRIMARY KEY," \
+                            "\"C0\" blob," \
+                            "\"C1\" blob," \
+                            "\"C2\" blob," \
+                            "\"C3\" blob," \
+                            "\"C3\" blob" \
+                            ") WITH COMPACT STORAGE" \
+                            "AND bloom_filter_fp_chance = 0.01" \
+                            "AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}" \
+                            "AND comment = ''" \
+                            "AND compaction = {'class': " \
+                            "'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32'," \
+                            " 'min_threshold': '4'}" \
+                            "AND compression = {'enabled': 'false'}" \
+                            "AND crc_check_chance = 1.0" \
+                            "AND dclocal_read_repair_chance = 0.1" \
+                            "AND default_time_to_live = 0" \
+                            "AND gc_grace_seconds = 864000" \
+                            "AND max_index_interval = 2048" \
+                            "AND memtable_flush_period_in_ms = 0" \
+                            "AND min_index_interval = 128" \
+                            "AND read_repair_chance = 0.0" \
+                            "AND speculative_retry = '99PERCENTILE';"
+            l.info("Create standard1 Table")
+            # Create 'standard1' & 'counter1' default Tables
+            session.execute(table_create)
+            l.info('Succeeded to create keyspace1 and standard1 Table.')
+            # Create Trigger
+            trigger_jar = 'org.apache.cassandra.triggers.AuditTrigger'
+            trigger_cql = "CREATE TRIGGER pushTrigger ON keyspace1.standard1 USING " + trigger_jar
+            session.execute(trigger_cql)
+        except Exception as e:
+            l.error('FAILED to create trigger. Error: %s' % str(e))
+
+    def reset_db(self):
+        try:
+            ips = self.options.cluster_ips.split(',')
+            cluster = Cluster(ips)
+            l.debug("Connecting to Cassandra Cluster: [%s]" % (ips))
+            session = cluster.connect()
+            l.info("dropping [keyspace1] (default) keyspace...")
+            # session.execute("DROP KEYSPACE keyspace1")
+            # Instead of 'dropping' the complete keyspace, let's delete all rows in Tables, so they remain created for subsequent tests
+            session.execute("TRUNCATE keyspace1.counter1;")
+            session.execute("TRUNCATE keyspace1.standard1;")
+            l.info('Succeeded to delete DB.')
+        except Exception as e:
+            l.error('Failed to reset Cassandra DB. Error: %s' % str(e))
 
     def stop_and_delete_all_apps(self):
         self.delete_all_launched_apps()
@@ -89,14 +176,14 @@ class RunTestCassandra(HydraBase):
             'max': [],           # Maximum latency in miliseconds.
             'gc_num': 0,         # Number of garbage collections.
             'max_ms': [],        # Longest garbage collection in miliseconds.
-            'sum_ms': 0,         # Total of gargage collection in miliseconds.
+            'sum_ms': 0,         # Total of garbage collection in miliseconds.
             'sdv_ms': [],        # Standard deviation in miliseconds.
             'mb': 0,             # Size of the garbage collection in megabytes.
             'op_time': []        # Total Operation Time per client
         }
         cassandra_results = {
-            'write': dict(result),
-            'read': dict(result)
+            'write': copy.deepcopy(result),
+            'read': copy.deepcopy(result)
         }
         # Get stats for Cassandra Stress Client
         stats = self.get_app_stats(self.stress_client)
@@ -104,25 +191,28 @@ class RunTestCassandra(HydraBase):
         db_ops = ['write', 'read']
         for client in stats.keys():
             info = stats[client]
-            # l.info(" CLIENT = " + pformat(client) + " DATA = " + pformat(info))
             for db_op in db_ops:
                 if db_op in info:
-                    info[db_op] = ast.literal_eval(info[db_op])
-                    cassandra_results[db_op]['total ops'].append(info[db_op]['Total partitions'])
-                    cassandra_results[db_op]['op/s'].append(info[db_op]['op rate'])
-                    cassandra_results[db_op]['pk/s'].append(info[db_op]['partition rate'])
-                    cassandra_results[db_op]['.95'].append(info[db_op]['latency 95th percentile'])
-                    cassandra_results[db_op]['.99'].append(info[db_op]['latency 99th percentile'])
-                    cassandra_results[db_op]['gc_num'] += int(info[db_op]['total gc count'])
-                    cassandra_results[db_op]['sdv_ms'].append(info[db_op]['stdev gc time(ms)'])
-                    cassandra_results[db_op]['max'].append(info[db_op]['latency max'])
-                    cassandra_results[db_op]['med'].append(info[db_op]['latency median'])
-                    cassandra_results[db_op]['op_time'].append(info[db_op]['Total operation time'])
+                    try:
+                        info[db_op] = ast.literal_eval(info[db_op])
+                        cassandra_results[db_op]['total ops'].append(int(info[db_op]['Total partitions']))
+                        cassandra_results[db_op]['op/s'].append(int(info[db_op]['op rate']))
+                        cassandra_results[db_op]['pk/s'].append(int(info[db_op]['partition rate']))
+                        cassandra_results[db_op]['.95'].append(float(info[db_op]['latency 95th percentile']))
+                        cassandra_results[db_op]['.99'].append(float(info[db_op]['latency 99th percentile']))
+                        cassandra_results[db_op]['gc_num'] += int(info[db_op]['total gc count'])
+                        cassandra_results[db_op]['sdv_ms'].append(float(info[db_op]['stdev gc time(ms)']))
+                        cassandra_results[db_op]['max'].append(float(info[db_op]['latency max']))
+                        cassandra_results[db_op]['med'].append(float(info[db_op]['latency median']))
+                        cassandra_results[db_op]['op_time'].append((info[db_op]['Total operation time']).replace(' ', ''))
+                    except Exception as e:
+                        l.error("Failed to parse stats from Client: " + pformat(client) + " DATA = " + pformat(info[db_op]))
+                        l.error("ERROR: %s" % str(e))
 
         return cassandra_results
 
     def launch_stress_client(self):
-        max_threads_per_client = 10
+        max_threads_per_client = 20
         l.info("Launching the Cassandra Stress Client(s). Total clients = %s" % (self.options.total_client_count))
         # Determine number of threads per Cassandra Stress Client
         if self.options.total_client_count > max_threads_per_client:
@@ -130,39 +220,100 @@ class RunTestCassandra(HydraBase):
         else:
             threads_per_client = self.options.total_client_count
         l.debug("Number of Threads per Cassandra-Stress Client, set to: %s" % (threads_per_client))
-        self.create_binary_app(name=self.stress_client, app_script='./src/stress_client.py %s %s %s %s %s'
+        self.create_binary_app(name=self.stress_client, app_script='./src/stress_client.py %s %s %s %s %s %s'
                                                                    % (self.options.total_ops_count,
                                                                       threads_per_client,
                                                                       self.options.cluster_ips,
+                                                                      self.options.test_duration,
                                                                       self.options.cl,
                                                                       self.options.profile),
-                               cpus=0.01, mem=32, ports=[0])
+                               cpus=0.2, mem=600, ports=[0])
         if self.options.total_client_count > max_threads_per_client:
-            client_count = math.ceil(self.options.total_client_count / 10)
+            client_count = math.ceil(self.options.total_client_count / max_threads_per_client)
             l.info("Number of Cassandra-Stress Clients to launch = %s" % (client_count))
             self.scale_and_verify_app(self.stress_client, client_count)
-
 
     def delete_all_launched_apps(self):
         l.info("Deleting Stress Clients")
         self.delete_app(self.stress_client)
 
 
+def simulate_node_failure(node_ips, max_duration, tests_completed):
+    """
+        Simulate random cassandra node failure and 'rejoin' into cluster
+    """
+    run = True
+    l.info("START Cassandra Node Failure Simulation. Entering.")
+    while run:
+        # If stress-tests are still running continue with node failure simulation
+        if not tests_completed.isSet():
+            # Select 'random' node from Cassandra Cluster
+            node_ip = select_random_node(node_ips)
+            # Determine delay before stopping cassandra node (to simulate failure / node down)
+            duration_secs = max_duration*60
+            time_next_stop = random.randint(1, duration_secs/4)
+            l.debug("STOP programmed in %s seconds" % time_next_stop)
+            # Wait
+            time.sleep(time_next_stop)
+            ssh_fail = False
+            # Stop Cassandra Node (simulate failure / stop the service)
+            stop_cmd = "sudo service cassandra stop"
+            l.debug("STOP Cassandra Node: %s"%node_ip)
+            try:
+                ssh = paramiko.SSHClient()
+                ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                ssh.connect(str(node_ip))
+                l.debug("[Simulate Cassandra Node Failure] Connected to host: %s" % node_ip)
+            except paramiko.AuthenticationException as e:
+                l.error("Authentication failed when connecting to %s. ERROR: %s" % (node_ip, e))
+                ssh_fail = True
+            except:
+                l.error("Could not SSH to %s, waiting for it to start" % node_ip)
+                ssh_fail = True
+            if not ssh_fail:
+                # Send the command to STOP cassandra node
+                ssh.exec_command(stop_cmd)
+                # Determine delay before starting cassandra node (to simulate rejoin to the cluster)
+                time_next_rejoin = random.randint(1, duration_secs/4)
+                l.debug("START programmed in %s seconds" % time_next_rejoin)
+                time.sleep(time_next_rejoin)
+                # Start Cassandra Node (simulate rejoin / start the service)
+                start_cmd = "sudo service cassandra start"
+                l.debug("START Cassandra Node: %s"%node_ip)
+                # Send the command (non-blocking)
+                ssh.exec_command(start_cmd)
+                # Disconnect from the host
+                l.debug("Closing SSH connection to host: %s" % node_ip)
+                ssh.close()
+                run=False
+        else:
+            # Tests Complete has been signaled
+            run=False
+            l.info("END node failure simulation. Exiting.")
+
+def select_random_node(cluster_ips):
+    """
+        Select a random cassandra node from a list of IPs
+    """
+    return random.choice(cluster_ips)
+
 class RunTest(object):
     def __init__(self, argv):
         usage = ('python %prog --test_duration=<time to run test> --total_ops_count=<Total Operations>'
                  '--total_client_count=<Total clients to launch> --cluster_ips=<cassandra node list ips>'
                  '--consistency_level=<cassandra consistency level> --profile=<yaml profile>'
-                 '--config_file=<path_to_config_file>')
+                 '--config_file=<path_to_config_file> --sim_failure=<simulate node failure>')
         parser = OptionParser(description='cassandra scale test master',
                               version="0.1", usage=usage)
-        parser.add_option("--test_duration", dest='test_duration', type='float', default=10)
+        parser.add_option("--test_duration", dest='test_duration', type='int', default=5)
         parser.add_option("--total_ops_count", dest='total_ops_count', type='int', default=1000000)
         parser.add_option("--total_client_count", dest='total_client_count', type='int', default=20)
         parser.add_option("--cluster_ips", dest='cluster_ips', type='string', default='127.0.0.1')
         parser.add_option("--consistency_level", dest='cl', type='string', default='LOCAL_ONE')
         parser.add_option("--profile", dest='profile', type='string', default='hydra_profile.yaml')
         parser.add_option("--config_file", dest='config_file', type='string', default='hydra.ini')
+        parser.add_option("--sim_failure", dest='sim_failure', action="store_true", default=False)
+
 
         (options, args) = parser.parse_args()
         # Check NO list of positional arguments leftover after parsing options
@@ -176,7 +327,8 @@ class RunTest(object):
         res = r.run_test()
         r.delete_all_launched_apps()
         # Cassandra-Stress Test Results
-        l.info("Cassandra Stress Results: \n%s" % pformat(res))
+        result_json = json.dumps(res)
+        print("Cassandra Stress Results: \n%s" % pformat(result_json))
         r.stop_appserver()
 
 if __name__ == "__main__":
--- src/stress_client.py
+++ src/stress_client.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
 __author__ = 'annyz'
+
+from sys import path
+# Append 'hydra' directory to Python path
+path.append("hydra/src/main/python")
 
 import logging
 import os
@@ -39,11 +44,15 @@ class HDCStressRepSrv(HDaemonRepSrv):
         """
         l.info('Scheduling Test to start %s.' % (start_time))
         start_time = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S.%f")
-        test_scheduler = BackgroundScheduler()
-        test_scheduler.add_job(lambda: time_to_start(self.run_data), next_run_time=start_time)
-        test_scheduler.start()
         self.run_data['test_status'] = 'running'
         self.run_data['stats']['msg_cnt'] = self.stress_metrics['ops_count']
+        # If time to start has passed, start immediately
+        if start_time < datetime.now():
+            time_to_start(self.run_data)
+        else:
+            test_scheduler = BackgroundScheduler()
+            test_scheduler.add_job(lambda: time_to_start(self.run_data), next_run_time=start_time)
+            test_scheduler.start()
         # Report Status
         return ('ok', None)
 
@@ -77,16 +86,44 @@ class HDCStressRepSrv(HDaemonRepSrv):
 
 def time_to_start(run_data):
     l.info('Starting Cassandra Stress Test at: %s' % (datetime.utcnow()))
-    process = psutil.Process()
     # Initialize test data collection
     run_data['start'] = True
-    run_data['stats']['write'] = {'net:start': json.dumps(psutil.net_io_counters()),
-                                  'cpu:start': json.dumps(process.cpu_times()),
-                                  'mem:start': json.dumps(process.memory_info()),
-                                  'time:start': json.dumps(time.time())
-                                  }
-    run_data['stats']['read'] = {}
     return
+
+
+def parse_results(stdout, run_data):
+    try:
+        l.info('Start parsing cassandra-stress output.')
+        index_start = stdout.find('op rate')
+        index_end = stdout.find('END')
+        if (index_start and index_end) != -1:
+            results = stdout[index_start:index_end]
+            # find 'matches' of the type att_name:value
+            matches = re.findall(r'.+\:{1}.+', results)
+            # partition each match at ':'
+            matches = [m.split(':', 1) for m in matches]
+            # Remove extra whitespaces
+            for pair in matches:
+                for idx, m in enumerate(pair):
+                    pair[idx] = re.sub(r'\s{2,}', '', m)
+            # Build dictionary of results
+            result_dict = dict(matches)
+            # Remove [] in results
+            for key, value in result_dict.iteritems():
+                result_dict[key] = re.sub(r'\[{1}.{1,}\]{1}', '', value)
+            run_data.update(result_dict)
+    except Exception, e:
+        l.error("ERROR while parsing Cassandra-Stress OUTPUT. Error: %s" % str(e))
+
+
+def check_user_queries(data_profile):
+    """
+    Check if 'user' queries are defined in YAML Profile.
+    :param data_profile: .yaml profile
+    :return: True if custom user queries are defined in profile, otherwise, False
+    """
+    # TODO: Analyze 'profile' and set 'user_queries'
+    return False
 
 
 def run(argv):
@@ -97,13 +134,14 @@ def run(argv):
     user_queries = False
 
     # Parse Inputs
-    if len(argv) > 5:
+    if len(argv) >= 6:
         ops_count = argv[1]             # number of Operations (e.g., n=1000000 insert/read one million rows)
         client_count = argv[2]          # client count (client threads)
         cluster_ips = argv[3]           # C*-Cluster IPs
-        # consistency_level = argv[4]     # consistency level, default='LOCAL_ONE'
-    if len(argv) > 6:
-        stress_profile = argv[4]        # (optional) .yaml profile (defines data model & queries)
+        duration = argv[4]
+        consistency_level = argv[5]     # consistency level, default='LOCAL_ONE'
+    if len(argv) >= 7:
+        stress_profile = argv[6]        # (optional) .yaml profile (defines data model & queries)
         user_queries = check_user_queries(stress_profile)
 
     # Initialize 'run_data'
@@ -127,42 +165,57 @@ def run(argv):
             continue
         l.info("START SIGNAL received. Cassandra Client initiating Stress Test...")
 
+        process = psutil.Process()
+        hd.run_data['stats']['write'] = {'net:start': json.dumps(psutil.net_io_counters()),
+                                         'cpu:start': json.dumps(process.cpu_times()),
+                                         'mem:start': json.dumps(process.memory_info()),
+                                         'time:start': json.dumps(time.time())
+                                        }
+
         # The cluster must be first populated by a 'write' test
-        w_start_time = time.time()              # write 'start' time
-        base_cmd = 'cassandra-stress %s n=%s -rate threads=%s -node %s'
-        write_cmd = base_cmd % ('write', ops_count, client_count,
+        base_cmd = 'cassandra-stress %s cl=%s duration=%sm -rate threads=%s -node %s -schema "replication(factor=2)"'
+        write_cmd = base_cmd % ('write', 'QUORUM', duration, client_count,
                                 cluster_ips)
 
-        # Initiate Subprocess Call (WRITE OPERATION)
-        l.info('Execute command: %s' % (write_cmd))
-        stress_process = subprocess.Popen(write_cmd, stdout=subprocess.PIPE,
-                                          stderr=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
-        stdout_w, stderr_w = stress_process.communicate()
-        cmd_status_w = stress_process.returncode
-        if cmd_status_w != 0:
-            l.error('Error while running Cassandra-Stress WRITE Operation. Return Code: %s' % (cmd_status_w))
-        l.debug('Output of Cassandra-Stress Test WRITE operation: %s' % (stdout_w))
-        l.debug('stderr of Cassandra-Stress Test WRITE operation: %s' % (stderr_w))
+        retries = 20
+        while retries > 0:
+            # Initiate Subprocess Call (WRITE OPERATION)
+            l.info('Execute command: %s' % (write_cmd))
+            stress_process = subprocess.Popen(write_cmd, stdout=subprocess.PIPE,
+                                              stderr=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+            stdout_w, stderr_w = stress_process.communicate()
+
+            cmd_status_w = stress_process.returncode
+            if cmd_status_w != 0:
+                l.error('Error while running Cassandra-Stress WRITE Operation. Return Code: %s' % (cmd_status_w))
+            l.debug('Output of Cassandra-Stress Test WRITE operation: %s' % (stdout_w))
+            l.debug('stderr of Cassandra-Stress Test WRITE operation: %s' % (stderr_w))
+
+            if 'NoHostAvailableException' in stderr_w:
+                retries = retries - 1
+                continue
+            else:
+                break
+
 
         # Write stats to 'run_data'
-        hd.run_data['stats']['write']['time:start'] = json.dumps(w_start_time)
         hd.run_data['stats']['write']['time:end'] = json.dumps(time.time())
-        parse_results(stdout_w, hd.run_data['stats']['write'])
-        l.debug("Updated RUN_DATA: \n%s" % pformat(hd.run_data))
-        process = psutil.Process()
         hd.run_data['stats']['write']['net:end'] = json.dumps(psutil.net_io_counters())
         hd.run_data['stats']['write']['cpu:end'] = json.dumps(process.cpu_times())
         hd.run_data['stats']['write']['mem:end'] = json.dumps(process.memory_info())
+        parse_results(stdout_w, hd.run_data['stats']['write'])
+        l.debug("Updated RUN_DATA: \n%s" % pformat(hd.run_data))
 
         # Initiate Subprocess Call (READ/QUERY OPERATION)
         if not user_queries:
-            query_cmd = base_cmd % ('read', ops_count, client_count,
+            query_cmd = base_cmd % ('read', 'ONE', duration, client_count,
                                     cluster_ips)
         else:
-            query_cmd = base_cmd % ('user', ops_count, client_count,
+            query_cmd = base_cmd % ('user', 'ONE', duration, client_count,
                                     cluster_ips)
             query_cmd += 'profile=%s' % (stress_profile)
 
+        hd.run_data['stats']['read'] = {}
         q_start_time = time.time()
         l.debug('Execute command: %s' % (query_cmd))
         stress_process = subprocess.Popen(query_cmd, stdout=subprocess.PIPE,
@@ -177,10 +230,10 @@ def run(argv):
         # Write stats to 'run_data'
         hd.run_data['stats']['read']['time:start'] = json.dumps(q_start_time)
         hd.run_data['stats']['read']['time:end'] = json.dumps(time.time())
-        parse_results(stdout_q, hd.run_data['stats']['read'])
         hd.run_data['stats']['read']['net:end'] = json.dumps(psutil.net_io_counters())
         hd.run_data['stats']['read']['cpu:end'] = json.dumps(process.cpu_times())
         hd.run_data['stats']['read']['mem:end'] = json.dumps(process.memory_info())
+        parse_results(stdout_q, hd.run_data['stats']['read'])
         hd.run_data['test_status'] = 'stopping'
         hd.run_data['start'] = False
         hd.run_data['msg_cnt'] = ops_count
@@ -189,35 +242,3 @@ def run(argv):
 
 if __name__ == "__main__":
     run(sys.argv)
-
-
-def parse_results(stdout, run_data):
-    try:
-        l.info('Start parsing cassandra-stress output.')
-        index_start = stdout.find('op rate')
-        index_end = stdout.find('END')
-        if (index_start and index_end) != -1:
-            results = stdout[index_start:index_end]
-            # find 'matches' of the type att_name:value
-            matches = re.findall(r'.+\:{1}.+', results)
-            # partition each match at ':'
-            matches = [m.split(':', 1) for m in matches]
-            # Remove extra whitespaces
-            for pair in matches:
-                for idx, m in enumerate(pair):
-                    pair[idx] = re.sub(r'\s{2,}', '', m)
-            # Build dictionary of results
-            result_dict = dict(matches)
-            run_data.update(result_dict)
-    except Exception, e:
-        l.error("ERROR while parsing Cassandra-Stress OUTPUT. Error: %s" % str(e))
-
-
-def check_user_queries(data_profile):
-    """
-    Check if 'user' queries are defined in YAML Profile.
-    :param data_profile: .yaml profile
-    :return: True if custom user queries are defined in profile, otherwise, False
-    """
-    # TODO: Analyze 'profile' and set 'user_queries'
-    return False
